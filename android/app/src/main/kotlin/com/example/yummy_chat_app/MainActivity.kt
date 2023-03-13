8772042a9fe25ff2b300c11d83a36bb29c1c02ae
smmne <kdaumh33@gmail.com>
2023-03-13 13:56:13 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/yummy_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yummy_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:yummy_chat/screen/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginSignupScreen(),
+    );
+  }
+}
